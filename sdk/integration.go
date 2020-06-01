c5e5c8bd92114bd69038be84cff6f760c0c29875
Jeff Haynie <jhaynie@pinpoint.com>
2020-06-01 18:22:54 -0500

    make the Logger and Model also aliases

--- sdk/integration.go
+++ sdk/integration.go
@@ -1,13 +1,9 @@
 package sdk
 
-import (
-	"github.com/pinpt/go-common/log"
-)
-
 // Integration is the interface that integrations implement
 type Integration interface {
 	// Start is called when the integration is starting up
-	Start(logger log.Logger, config Config, manager Manager) error
+	Start(logger Logger, config Config, manager Manager) error
 	// Export is called to tell the integration to run an export
 	Export(export Export) error
 	// WebHook is called when a webhook is received on behalf of the integration
--- sdk/pipe.go
+++ sdk/pipe.go
@@ -1,11 +1,9 @@
 package sdk
 
-import "github.com/pinpt/go-common/datamodel"
-
 // Pipe for sending data back to pinpoint
 type Pipe interface {
 	// Write a model back to the output system
-	Write(object datamodel.Model) error
+	Write(object Model) error
 	// Flush will tell the pipe to flush any pending data
 	Flush() error
 	// Close is called when the integration has completed and no more data will be sent
