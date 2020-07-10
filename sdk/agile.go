a1a8da871259673b35ada6eef5c56e7e753d912d
Jeff Haynie <jhaynie@pinpoint.com>
2020-07-10 16:29:32 -0500

    updates for agile model changes

--- sdk/agile.go
+++ sdk/agile.go
@@ -1,73 +1,73 @@
 package sdk
 
-import "github.com/pinpt/integration-sdk/work/agile"
+import "github.com/pinpt/integration-sdk/work"
 
 // Type aliases for our exported datamodel types to create a stable version
 // which Integrations depend on instead of directly depending on a specific
 // version of the integration-sdk directly
 
 // AgileBoard is the agile board
-type AgileBoard = agile.Board
+type AgileBoard = work.Board
 
 // AgileBoardType is the board type
-type AgileBoardType = agile.BoardType
+type AgileBoardType = work.BoardType
 
 // AgileBoardTypeScrum is the enumeration value for scrum
-const AgileBoardTypeScrum = agile.BoardTypeScrum
+const AgileBoardTypeScrum = work.BoardTypeScrum
 
 // AgileBoardTypeKanban is the enumeration value for kanban
-const AgileBoardTypeKanban = agile.BoardTypeKanban
+const AgileBoardTypeKanban = work.BoardTypeKanban
 
 // AgileBoardTypeOther is the enumeration value for other
-const AgileBoardTypeOther = agile.BoardTypeOther
+const AgileBoardTypeOther = work.BoardTypeOther
 
 // AgileKanban is a kanban board
-type AgileKanban = agile.Kanban
+type AgileKanban = work.Kanban
 
 // AgileSprint is a sprint board
-type AgileSprint = agile.Sprint
+type AgileSprint = work.Sprint
 
 // AgileBoardColumns is the columns for an agile board
-type AgileBoardColumns = agile.BoardColumns
+type AgileBoardColumns = work.BoardColumns
 
 // AgileKanbanColumns is the columns for a kanban board
-type AgileKanbanColumns = agile.KanbanColumns
+type AgileKanbanColumns = work.KanbanColumns
 
 // AgileSprintClosedDate is the sprint closed date
-type AgileSprintClosedDate = agile.SprintClosedDate
+type AgileSprintClosedDate = work.SprintCompletedDate
 
 // AgileSprintEndDate is the sprint planned end date
-type AgileSprintEndDate = agile.SprintEndDate
+type AgileSprintEndDate = work.SprintEndedDate
 
 // AgileSprintStartDate is the sprint planned start date
-type AgileSprintStartDate = agile.SprintStartDate
+type AgileSprintStartDate = work.SprintStartedDate
 
 // AgileSprintColumns is the sprint columns
-type AgileSprintColumns = agile.SprintColumns
+type AgileSprintColumns = work.SprintColumns
 
 // AgileSprintStatus is the sprint status
-type AgileSprintStatus = agile.SprintStatus
+type AgileSprintStatus = work.SprintStatus
 
 // AgileSprintStatusClosed is the enumeration value for closed
-const AgileSprintStatusClosed = agile.SprintStatusClosed
+const AgileSprintStatusClosed = work.SprintStatusClosed
 
 // AgileSprintStatusActive is the enumeration value for active
-const AgileSprintStatusActive = agile.SprintStatusActive
+const AgileSprintStatusActive = work.SprintStatusActive
 
 // AgileSprintStatusFuture is the enumeration value for future
-const AgileSprintStatusFuture = agile.SprintStatusFuture
+const AgileSprintStatusFuture = work.SprintStatusFuture
 
 // NewAgileBoardID will return the agile board id
 func NewAgileBoardID(customerID string, refID string, refType string) string {
-	return agile.NewBoardID(customerID, refID, refType)
+	return work.NewBoardID(customerID, refID, refType)
 }
 
 // NewAgileKanbanID will return the agile kanban id
 func NewAgileKanbanID(customerID string, refID string, refType string) string {
-	return agile.NewKanbanID(customerID, refID, refType)
+	return work.NewKanbanID(customerID, refID, refType)
 }
 
 // NewAgileSprintID will return the agile sprint id
 func NewAgileSprintID(customerID string, refID string, refType string) string {
-	return agile.NewSprintID(customerID, refID, refType)
+	return work.NewSprintID(customerID, refID, refType)
 }
